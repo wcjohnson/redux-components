ea6b52e2db4b31eda19eb3707af53bb21d00ad22
William C. Johnson <wcjohnson@oigroup.net>
2016-03-17 01:57:07 -0400

    Fix some exports things

---
+++ index.js
@@ -0,0 +1,8 @@
+// Default exports for redux-components
+module.exports = {
+	createClass: require('./createClass.js'),
+	createComponent: require('./createComponent.js'),
+	mountComponent: require('./mountComponent.js'),
+	ReduxComponent: require('./ReduxComponent.js'),
+	SubtreeMixin: require('./SubtreeMixin.js')
+};
